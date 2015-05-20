d39516c29525a7772d27ccd6c4eb455f0a2b6a5e
Pavel Pavlenko <region23@gmail.com>
2015-05-20 14:31:58 +0300

    msg=Translated example Variables

--- examples/variables/variables.go
+++ examples/variables/variables.go
@@ -1,6 +1,6 @@
-// In Go, _variables_ are explicitly declared and used by
-// the compiler to e.g. check type-correctness of function
-// calls.
+// В Go, _переменные_ объявляются явно и используются
+// компилятором, для проверки корректности
+// типов вызываемых функций.
 
 package main
 
@@ -8,27 +8,26 @@ import "fmt"
 
 func main() {
 
-    // `var` declares 1 or more variables.
+    // `var` объявляет 1 или более переменных.
     var a string = "initial"
     fmt.Println(a)
 
-    // You can declare multiple variables at once.
+    // Вы можете объявить несколько переменных за раз.
     var b, c int = 1, 2
     fmt.Println(b, c)
 
-    // Go will infer the type of initialized variables.
+    // Go определит тип инициализированных переменных автоматически.
     var d = true
     fmt.Println(d)
 
-    // Variables declared without a corresponding
-    // initialization are _zero-valued_. For example, the
-    // zero value for an `int` is `0`.
+    // Не инициализированные переменные имеют _нулевое значение_.
+    // Например нулевое значение для `int` это 0.
     var e int
     fmt.Println(e)
 
-    // The `:=` syntax is shorthand for declaring and
-    // initializing a variable, e.g. for
-    // `var f string = "short"` in this case.
+    // Синтаксис `:=` это сокращение для объявления
+    // и инициализации переменной, например
+    // в этом примере `var f string = "short"`.
     f := "short"
     fmt.Println(f)
 }
