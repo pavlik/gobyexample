22593c9e3f075af78acb41957aedea98965115b3
Pavel Pavlenko <region23@gmail.com>
2015-05-20 12:27:24 +0300

    INIT: initial commit on gh-pages branch

--- examples/hello-world/hello-world.go
+++
@@ -1,9 +0,0 @@
-// Наша первая программа будет выводить в консоль
-// классическую фразу "hello world". Вот весь код программы.
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("hello world")
-}
