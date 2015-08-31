98afdd4124ac4e478f34c6d140f6c4b7841c95b7
marat <marat.art@gmail.com>
2015-08-31 22:58:17 +0300

    Rus: Range over Channels

--- examples/range-over-channels/range-over-channels.sh
+++ examples/range-over-channels/range-over-channels.sh
@@ -2,6 +2,6 @@ $ go run range-over-channels.go
 one
 two
 
-# This example also showed that it's possible to close
-# a non-empty channel but still have the remaining
-# values be received.
+# Этот пример также показывает возможность закрытия
+# не пустого канала, всё ещё имеющего оставшиеся
+# принимаемые значения.
