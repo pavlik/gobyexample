4714eb1620b72505c47eb44ef8973cfc3c780050
marat <marat.art@gmail.com>
2015-08-31 22:18:27 +0300

    Tranlated to Russian: Closing Channels

--- examples/closing-channels/closing-channels.go
+++ examples/closing-channels/closing-channels.go
@@ -1,50 +1,64 @@
-// _Closing_ a channel indicates that no more values
-// will be sent on it. This can be useful to communicate
-// completion to the channel's receivers.
+// _Закрытие_ канала показывает, что больше нельзя
+// будет отправить значения через него.
+// Это может быть полезно для сообщения о завершении
+// приёмникам канала.
 
 package main
 
 import "fmt"
 
-// In this example we'll use a `jobs` channel to
-// communicate work to be done from the `main()` goroutine
-// to a worker goroutine. When we have no more jobs for
-// the worker we'll `close` the `jobs` channel.
+// В этом примере мы используем канал `jobs` для
+// сообщения о завершении работы от горутины `main()`
+// рабочей горутине. Когда у нас больше нет задач для
+// рабочей горутины мы закрываем канал `jobs`.
+
 func main() {
-    jobs := make(chan int, 5)
-    done := make(chan bool)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-    // Here's the worker goroutine. It repeatedly receives
-    // from `jobs` with `j, more := <-jobs`. In this
-    // special 2-value form of receive, the `more` value
-    // will be `false` if `jobs` has been `close`d and all
-    // values in the channel have already been received.
-    // We use this to notify on `done` when we've worked
-    // all our jobs.
-    go func() {
-        for {
-            j, more := <-jobs
-            if more {
-                fmt.Println("received job", j)
-            } else {
-                fmt.Println("received all jobs")
-                done <- true
-                return
-            }
-        }
-    }()
+	// Here's the worker goroutine. It repeatedly receives
+	// from `jobs` with `j, more := <-jobs`. In this
+	// special 2-value form of receive, the `more` value
+	// will be `false` if `jobs` has been `close`d and all
+	// values in the channel have already been received.
+	// We use this to notify on `done` when we've worked
+	// all our jobs.
 
-    // This sends 3 jobs to the worker over the `jobs`
-    // channel, then closes it.
-    for j := 1; j <= 3; j++ {
-        jobs <- j
-        fmt.Println("sent job", j)
-    }
-    close(jobs)
-    fmt.Println("sent all jobs")
+	// Это рабочая горутина. Она постоянно получает данные из
+	// `jobs` посредством `j, more := <-jobs`. В этой
+	// специальной форме получения данных с 2-мя значениями,
+	// значение `more` будет `false` если `jobs` был закрыт
+	// и все значения в канале уже были получены.
+	// Мы используем этот подход для уведомления на
+	// `done` в момент, когда обработаны все задачи.
 
-    // We await the worker using the
-    // [synchronization](channel-synchronization) approach
-    // we saw earlier.
-    <-done
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	// Отправляем 3 задачи рабочей горутине через
+	// канал `jobs` и закрываем его.
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	// We await the worker using the
+	// [synchronization](channel-synchronization) approach
+	// we saw earlier.
+	// Ждем рабочую горутину используя подход
+	// [синхронизации](channel-synchronization),
+	// который был рассмотрен ранее.
+	<-done
 }
--- examples/closing-channels/closing-channels.sh
+++ examples/closing-channels/closing-channels.sh
@@ -1,4 +1,4 @@
-$ go run closing-channels.go 
+$ go run closing-channels.go
 sent job 1
 received job 1
 sent job 2
@@ -10,3 +10,5 @@ received all jobs
 
 # The idea of closed channels leads naturally to our next
 # example: `range` over channels.
+# Идея закрытых каналов естественно приводит к следующему
+# примеру: `range` через каналы.
