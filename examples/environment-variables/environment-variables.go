0dc11a7a023d7684c38b7fadc62a01c4823851f3
Ivan Daniluk <ivan.daniluk@gmail.com>
2015-08-31 17:59:41 -0400

    Added translation for env variables

--- examples/environment-variables/environment-variables.go
+++ examples/environment-variables/environment-variables.go
@@ -1,7 +1,8 @@
-// [Environment variables](http://en.wikipedia.org/wiki/Environment_variable)
-// are a universal mechanism for [conveying configuration
-// information to Unix programs](http://www.12factor.net/config).
-// Let's look at how to set, get, and list environment variables.
+// [Переменные окружения](http://en.wikipedia.org/wiki/Environment_variable)
+// это универсальный механизм для [передачи конфигурации в UNIX
+// программы](http://www.12factor.net/config).
+// Давайте научимся устанавливать, считывать и просматривать
+// переменные окружения в Go.
 
 package main
 
@@ -11,21 +12,20 @@ import "fmt"
 
 func main() {
 
-    // To set a key/value pair, use `os.Setenv`. To get a
-    // value for a key, use `os.Getenv`. This will return
-    // an empty string if the key isn't present in the
-    // environment.
-    os.Setenv("FOO", "1")
-    fmt.Println("FOO:", os.Getenv("FOO"))
-    fmt.Println("BAR:", os.Getenv("BAR"))
+	// Чтобы присвоить значение переменной, используйте `os.Setenv`.
+	// Для получения значения - `os.Getenv`. Эта функция вернет
+	// пустую строку, если такой переменной нет в окружении.
+	os.Setenv("FOO", "1")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
 
-    // Use `os.Environ` to list all key/value pairs in the
-    // environment. This returns a slice of strings in the
-    // form `KEY=value`. You can `strings.Split` them to
-    // get the key and value. Here we print all the keys.
-    fmt.Println()
-    for _, e := range os.Environ() {
-        pair := strings.Split(e, "=")
-        fmt.Println(pair[0])
-    }
+	// Используйте `os.Environ`, чтобы вывести все существующие
+	// переменные и их значения. Эта функция возвращает срез строк в
+	// формате `КЛЮЧ=значение`. Вы можете `strings.Split`-нуть их,
+	// чтобы разделить на ключ и значение. Тут мы выводим все ключи на экран.
+	fmt.Println()
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0])
+	}
 }
