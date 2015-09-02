73604956a8783a38d88c0a5abe0f710759bc4b3d
Pavel Pavlenko <region23@gmail.com>
2015-09-02 20:56:51 +0300

    rebuilding site среда,  2 сентября 2015 г. 20:56:51 (MSK)

--- examples/non-blocking-channel-operations/non-blocking-channel-operations.go
+++ examples/non-blocking-channel-operations/non-blocking-channel-operations.go
@@ -10,41 +10,41 @@ import "fmt"
 
 func main() {
 
-	messages := make(chan string)
-	signals := make(chan bool)
+    messages := make(chan string)
+    signals := make(chan bool)
 
-	// Здесь показан неблокирующий приём.
-	// Если значение доступно
-	// в `messages`, то `select` выберет вариант
-	// `<-messages` с этим значением. Если нет,
-	// то будет сразу выбран вариант `default`.
-	select {
-	case msg := <-messages:
-		fmt.Println("received message", msg)
-	default:
-		fmt.Println("no message received")
-	}
+    // Здесь показан неблокирующий приём.
+    // Если значение доступно
+    // в `messages`, то `select` выберет вариант
+    // `<-messages` с этим значением. Если нет,
+    // то будет сразу выбран вариант `default`.
+    select {
+    case msg := <-messages:
+        fmt.Println("received message", msg)
+    default:
+        fmt.Println("no message received")
+    }
 
-	// Неблокирующая отправка работает аналогично.
-	msg := "hi"
-	select {
-	case messages <- msg:
-		fmt.Println("sent message", msg)
-	default:
-		fmt.Println("no message sent")
-	}
+    // Неблокирующая отправка работает аналогично.
+    msg := "hi"
+    select {
+    case messages <- msg:
+        fmt.Println("sent message", msg)
+    default:
+        fmt.Println("no message sent")
+    }
 
-	// Можно использовать несколько `case` перед
-	// оператором `default` для реализации многовариантного
-	// неблокирующего выбора. Здесь показана попытка
-	// неблокирующим способом получить
-	// как `messages` так и `signals`.
-	select {
-	case msg := <-messages:
-		fmt.Println("received message", msg)
-	case sig := <-signals:
-		fmt.Println("received signal", sig)
-	default:
-		fmt.Println("no activity")
-	}
+    // Можно использовать несколько `case` перед
+    // оператором `default` для реализации многовариантного
+    // неблокирующего выбора. Здесь показана попытка
+    // неблокирующим способом получить
+    // как `messages` так и `signals`.
+    select {
+    case msg := <-messages:
+        fmt.Println("received message", msg)
+    case sig := <-signals:
+        fmt.Println("received signal", sig)
+    default:
+        fmt.Println("no activity")
+    }
 }
