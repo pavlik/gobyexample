a38486e835eb255e4d187b1a9585a6e999c69613
marat <marat.art@gmail.com>
2015-09-02 12:37:53 +0300

    Rus: Timers

--- examples/timers/timers.sh
+++ examples/timers/timers.sh
@@ -1,6 +1,5 @@
-// The first timer will expire ~2s after we start the
-// program, but the second should be stopped before it has
-// a chance to expire.
+# Первый таймер закончится ~2 сек. после старта программы.
+# Второй должен остановится до истечения срока таймера.
 $ go run timers.go
 Timer 1 expired
 Timer 2 stopped
