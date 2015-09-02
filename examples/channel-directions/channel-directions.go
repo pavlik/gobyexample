8442d6ead02d5fc7c65c39c96df10c879d9669da
marat <marat.art@gmail.com>
2015-09-02 21:21:03 +0300

    Small correction

--- examples/channel-directions/channel-directions.go
+++ examples/channel-directions/channel-directions.go
@@ -1,5 +1,5 @@
 // При использовании каналов в качестве параметров функции
-// вы можете указать, будет ли канал предназначен только
+// можно указать, будет ли канал предназначен только
 // для отправки или только для получения значений. Это
 // указание повышает безопасность типов программы.
 
@@ -7,24 +7,24 @@ package main
 
 import "fmt"
 
-// Функция `ping` принимает только канал для отправки
-// значений. Попытка получения в этом канале вызовет
+// Функция `ping` принимает канал только для отправки
+// значений. Попытка приёма в этом канале вызовет
 // ошибку компиляции.
 func ping(pings chan<- string, msg string) {
-    pings <- msg
+	pings <- msg
 }
 
-// Функция `pong` принимает один канал для получения
+// Функция `pong` принимает один канал для приёма
 // (`pings`) и второй для отправки (`pongs`).
 func pong(pings <-chan string, pongs chan<- string) {
-    msg := <-pings
-    pongs <- msg
+	msg := <-pings
+	pongs <- msg
 }
 
 func main() {
-    pings := make(chan string, 1)
-    pongs := make(chan string, 1)
-    ping(pings, "passed message")
-    pong(pings, pongs)
-    fmt.Println(<-pongs)
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
