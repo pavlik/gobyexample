6f321763bb5ee1d27bc1be256bd999fdd48ce4ce
izdi <izdieu@gmail.com>
2015-09-20 18:03:31 +0300

    Defer Collection Functions String Functions String Formatting

--- examples/defer/defer.go
+++ examples/defer/defer.go
@@ -1,44 +1,43 @@
-// _Defer_ is used to ensure that a function call is
-// performed later in a program's execution, usually for
-// purposes of cleanup. `defer` is often used where e.g.
-// `ensure` and `finally` would be used in other languages.
+// _Defer_ гарантирует отложенный вызов функциии, обычно
+// отчистки,  по мере выполнения программы. `defer` часто
+// используется подобно `ensure` и `finally` в других языках.
 
 package main
 
 import "fmt"
 import "os"
 
-// Suppose we wanted to create a file, write to it,
-// and then close when we're done. Here's how we could
-// do that with `defer`.
+// Предположим, мы хотели бы создать файл, записать в него
+// данные и затем закрыть его, когда закончим. Вот, что мы
+// могли бы сделать с `defer`.
 func main() {
 
-    // Immediately after getting a file object with
-    // `createFile`, we defer the closing of that file
-    // with `closeFile`. This will be executed at the end
-    // of the enclosing function (`main`), after
-    // `writeFile` has finished.
-    f := createFile("/tmp/defer.txt")
-    defer closeFile(f)
-    writeFile(f)
+	// После получения объекта файла с помощью функции
+	// `createFile`, мы откладываем закрытие файла с
+	// помощью функции `closeFile`. Выполнение произойдет
+	// в конце блока текущей функции (`main`) после того,
+	// как `writeFile` выполнит свою работу.
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
 }
 
 func createFile(p string) *os.File {
-    fmt.Println("creating")
-    f, err := os.Create(p)
-    if err != nil {
-        panic(err)
-    }
-    return f
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
 
 func writeFile(f *os.File) {
-    fmt.Println("writing")
-    fmt.Fprintln(f, "data")
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
 
 }
 
 func closeFile(f *os.File) {
-    fmt.Println("closing")
-    f.Close()
+	fmt.Println("closing")
+	f.Close()
 }
--- examples/string-functions/string-functions.go
+++ examples/string-functions/string-functions.go
@@ -1,43 +1,43 @@
-// The standard library's `strings` package provides many
-// useful string-related functions. Here are some examples
-// to give you a sense of the package.
+// Пакет стандартной библиотеки `strings` предоставляет много
+// полезных функций для работы со строками. Вот, несколько примеров,
+// отображающие возможности пакета.
 
 package main
 
 import s "strings"
 import "fmt"
 
-// We alias `fmt.Println` to a shorter name as we'll use
-// it a lot below.
+// Присвоим переменной `p` функцию `fmt.Println` для сокращения
+// имени, которую мы будем использовать ниже.
 var p = fmt.Println
 
 func main() {
 
-    // Here's a sample of the functions available in
-    // `strings`. Note that these are all functions from
-    // package, not methods on the string object itself.
-    // This means that we need pass the string in question
-    // as the first argument to the function.
-    p("Contains:  ", s.Contains("test", "es"))
-    p("Count:     ", s.Count("test", "t"))
-    p("HasPrefix: ", s.HasPrefix("test", "te"))
-    p("HasSuffix: ", s.HasSuffix("test", "st"))
-    p("Index:     ", s.Index("test", "e"))
-    p("Join:      ", s.Join([]string{"a", "b"}, "-"))
-    p("Repeat:    ", s.Repeat("a", 5))
-    p("Replace:   ", s.Replace("foo", "o", "0", -1))
-    p("Replace:   ", s.Replace("foo", "o", "0", 1))
-    p("Split:     ", s.Split("a-b-c-d-e", "-"))
-    p("ToLower:   ", s.ToLower("TEST"))
-    p("ToUpper:   ", s.ToUpper("test"))
-    p()
+	// Данные функции доступны в пакете `strings`.
+	// Обратите внимание, что все эти функции из пакета, а
+	// не методы строковых объектов. Это означает, что нам
+	// необходимо передать первым аргументом функции, строку,
+	// над которой мы производим операцию.
+	p("Contains:  ", s.Contains("test", "es"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 1))
+	p("Split:     ", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
+	p()
 
-    // You can find more functions in the [`strings`](http://golang.org/pkg/strings/)
-    // package docs.
+	// Вы можете найти больше функций из документации
+	// к пакету [`strings`](http://golang.org/pkg/strings/)
 
-    // Not part of `strings` but worth mentioning here are
-    // the mechanisms for getting the length of a string
-    // and getting a character by index.
-    p("Len: ", len("hello"))
-    p("Char:", "hello"[1])
+	// Примеры ниже не относятся к пакету `strings`, но о них
+	// стоит упомянуть - это механизмы для получения длины строки
+	// и получение символа по индексу.
+	p("Len: ", len("hello"))
+	p("Char:", "hello"[1])
 }
